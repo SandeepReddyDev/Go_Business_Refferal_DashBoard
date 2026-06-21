d667e4da9358a1e42fc006c50a131604c5e9741b
SandeepReddy-dev <Sandeepreddydaram779@gmail.com>
2026-06-21 22:29:03 +0530

    Updated few modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOMClient.createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
